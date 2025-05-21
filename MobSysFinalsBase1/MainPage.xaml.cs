f7bd83e184a17732471761771346b32145a8a4fe
Erwin Mendoza <khronosangel@gmail.com>
2025-05-21 10:12:03 +0800

    Add project files.

---
+++ MobSysFinalsBase1/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MobSysFinalsBase1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MobSysFinalsBase1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MobSysFinalsBase1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
